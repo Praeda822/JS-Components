0c87f9b1697ee100587612970a10d8a3fe91d2d2
Patrick Kelly <patdkelly5@gmail.com>
2024-03-14 15:35:46 +1100

    Finished tabs project

---
+++ Tabs/app.js
@@ -0,0 +1,24 @@
+"use strict";
+const about = document.querySelector(".about");
+const btns = document.querySelectorAll(".tab-btn");
+const articles = document.querySelectorAll(".content");
+
+about.addEventListener("click", function (e) {
+  const id = e.target.dataset.id;
+  if (id) {
+    // Remove active from other buttons
+    btns.forEach(function (btn) {
+      btn.classList.remove("active");
+    });
+    // Outside the loop
+    // Add active class to clicked button
+    e.target.classList.add("active");
+    // Hide other articles
+    articles.forEach(function (article) {
+      article.classList.remove("active");
+    });
+    // Show active article
+    const element = document.getElementById(id);
+    element.classList.add("active");
+  }
+});
