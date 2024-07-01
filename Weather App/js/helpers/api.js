1926d7293b38f94c65bc78a84e95ffdcec3de9a0
Patrick Kelly <patdkelly5@gmail.com>
2024-07-01 17:36:41 +1000

    Rolled back MVC implementation - FUCK MVC and the fucking gronk who came up with it

--- Weather App/js/helpers/api.js
+++
@@ -1,6 +0,0 @@
-export const getJSON = function (url, errorMsg = "Something went wrong..") {
-  return fetch(url).then((response) => {
-    if (!response.ok) throw new Error(`${errorMsg} (${response.status})`);
-    return response.json();
-  });
-};
