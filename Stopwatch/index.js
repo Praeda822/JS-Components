1777045286b95adb3c562f1652adc8b4eab17d5f
Patrick Kelly <patdkelly5@gmail.com>
2024-07-02 20:11:45 +1000

    Basic javscript function structure

--- Stopwatch/index.js
+++ Stopwatch/index.js
@@ -1,1 +1,13 @@
 "use strict";
+
+const display = document.getElementById("display");
+let timer = null;
+let startTime = 0;
+let elapsedTime = 0;
+let isRunning = false;
+
+function start() {}
+
+function stop() {}
+
+function reset() {}
