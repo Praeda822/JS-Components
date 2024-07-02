03079f029d4c0f217b959a654cda9526af2bda99
Patrick Kelly <patdkelly5@gmail.com>
2024-07-02 21:19:47 +1000

    working digital clock...

--- Digital Clock/index.js
+++ Digital Clock/index.js
@@ -1,1 +1,12 @@
 "use strict";
+
+function updateClock() {
+  const now = new Date();
+  const hours = now.getHours().toString();
+  const minutes = now.getMinutes().toString();
+  const seconds = now.getSeconds().toString();
+  const timeString = `${hours}:${minutes}:${seconds}`;
+  document.getElementById("clock").innerHTML = timeString;
+}
+
+updateClock();
